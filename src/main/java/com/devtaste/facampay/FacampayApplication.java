b38b4e56c948f30ffcc7e68b665fe6c6e349ec5f
이호준 <hjlee@kai-i.com>
2023-11-30 22:14:09 +0900

    Initialize Spring

---
+++ src/main/java/com/devtaste/facampay/FacampayApplication.java
@@ -0,0 +1,13 @@
+package com.devtaste.facampay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacampayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacampayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devtaste/facampay/FacampayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtaste.facampay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacampayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
