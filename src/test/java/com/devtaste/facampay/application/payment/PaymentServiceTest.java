b79def96c6a53ccfefb2957dca3bb783cbe4b56d
이호준 <psh10066@gmail.com>
2023-12-04 19:37:13 +0900

    서비스 테스트 단위 테스트로 변경

--- src/test/java/com/devtaste/facampay/application/payment/PaymentServiceTest.java
+++ src/test/java/com/devtaste/facampay/application/payment/PaymentServiceTest.java
@@ -3,42 +3,51 @@ package com.devtaste.facampay.application.payment;
 import com.devtaste.facampay.domain.model.payment.Payment;
 import com.devtaste.facampay.domain.model.payment.PaymentRepository;
 import com.devtaste.facampay.domain.model.payment.type.PaymentStatusType;
-import com.devtaste.facampay.infrastructure.helper.PaymentTestHelper;
+import com.devtaste.facampay.domain.model.store.Store;
+import com.devtaste.facampay.domain.model.store.StoreRepository;
+import com.devtaste.facampay.domain.model.user.User;
+import com.devtaste.facampay.domain.model.user.UserRepository;
 import com.devtaste.facampay.presentation.store.request.PostPaymentRequest;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
-public class PaymentServiceTest extends PaymentTestHelper {
+@ExtendWith(MockitoExtension.class)
+public class PaymentServiceTest {
 
-    @Autowired
-    private PaymentRepository paymentRepository;
-
+    @InjectMocks
     private PaymentService paymentService;
 
-    @BeforeAll
-    void before() {
-        super.preparePaymentTest();
-
-        paymentService = new PaymentService(super.getStoreRepository(), super.getUserRepository(), paymentRepository);
-    }
+    @Mock
+    private StoreRepository storeRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PaymentRepository paymentRepository;
 
     @DisplayName("결제 요청")
     @Test
     void postPayment() {
-        PostPaymentRequest request = new PostPaymentRequest(super.getStore().getStoreId(), super.getUser().getUserId(), 10000L);
+        PostPaymentRequest request = new PostPaymentRequest(1L, 2L, 10000L);
+        Optional<Store> store = Optional.of(Store.of("store@facam.com", "가맹점1", 0L));
+        Optional<User> user = Optional.of(User.of("user@facam.com", "사용자1", 25000L));
+        given(storeRepository.findById(request.getStoreId())).willReturn(store);
+        given(userRepository.findById(request.getUserId())).willReturn(user);
+        given(paymentRepository.save(any(Payment.class))).willReturn(Payment.of(store.get(), user.get(), request.getMoney(), PaymentStatusType.WAITING));
+
         paymentService.postPayment(request);
 
-        Optional<Payment> payment = paymentRepository.findFirstByStoreStoreIdAndUserUserIdOrderByPaymentIdDesc(request.getStoreId(), request.getUserId());
-
-        assertTrue(payment.isPresent());
-        assertEquals(payment.get().getMoney(), request.getMoney());
-        assertEquals(payment.get().getPaymentStatus(), PaymentStatusType.WAITING);
+        then(storeRepository).should().findById(request.getStoreId());
+        then(userRepository).should().findById(request.getUserId());
+        then(paymentRepository).should().save(any(Payment.class));
     }
 }
--- src/test/java/com/devtaste/facampay/domain/model/payment/PaymentRepositoryTest.java
+++ src/test/java/com/devtaste/facampay/domain/model/payment/PaymentRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.devtaste.facampay.domain.model.payment;
 
 import com.devtaste.facampay.domain.model.payment.type.PaymentStatusType;
-import com.devtaste.facampay.infrastructure.helper.PaymentTestHelper;
+import com.devtaste.facampay.domain.model.store.Store;
+import com.devtaste.facampay.domain.model.store.StoreRepository;
+import com.devtaste.facampay.domain.model.user.User;
+import com.devtaste.facampay.domain.model.user.UserRepository;
+import com.devtaste.facampay.infrastructure.helper.RepositoryTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,14 +14,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class PaymentRepositoryTest extends PaymentTestHelper {
+public class PaymentRepositoryTest extends RepositoryTest {
 
     @Autowired
     private PaymentRepository paymentRepository;
+    @Autowired
+    private StoreRepository storeRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    private Store store;
+    private User user;
 
     @BeforeAll
     void before() {
-        super.preparePaymentTest();
+        this.storeRepository.deleteAllInBatch();
+        this.userRepository.deleteAllInBatch();
+        this.store = this.storeRepository.save(Store.of("store@facam.com", "가맹점1", 0L));
+        this.user = this.userRepository.save(User.of("user@facam.com", "사용자1", 25000L));
     }
 
     @DisplayName("결제 저장")
@@ -25,7 +39,7 @@ public class PaymentRepositoryTest extends PaymentTestHelper {
     void save() {
         long beforeCount = paymentRepository.count();
 
-        Payment payment = paymentRepository.save(Payment.of(super.getStore(), super.getUser(), 10000L, PaymentStatusType.WAITING));
+        Payment payment = paymentRepository.save(Payment.of(this.store, this.user, 10000L, PaymentStatusType.WAITING));
 
         long afterCount = paymentRepository.count();
 
